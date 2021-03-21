de97be2ba290f6d96a844b4019690eaf7d1243cb
Soulaimane NG <med.ng.1965@gmail.com>
2021-03-21 02:19:02 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component{
       );
     return (
       <div className="App">
+        <h1> Monsteeeer ! </h1>
         <SearchBar placeholder="Search Monsters" handleChange={
           e => this.setState(
             {searchField: e.target.value},()=>{
